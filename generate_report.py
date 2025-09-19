e16d487598713a209da4e83d9808a7c5646dd903
MerolaMagdy <meroomagdy165@gmail.com>
2025-09-19 15:19:05 +0300

    Update generate_report.py

--- generate_report.py
+++ generate_report.py
@@ -1,9 +1,21 @@
 import json
+from analysis import run_analysis
 import mailparser
 
 from test_mailparser import analyze_headers   # الدوال اللي عملناها قبل كده
 from test_mailparser import analyze_keywords # لو حطيناها في ملف مستقل
 from test_mailparser import analyze_links       # لو عندنا فحص روابط
+
+
+def generate_report(file_path):
+    report = run_analysis(file_path)
+    with open("report.json", "w", encoding="utf-8") as f:
+        json.dump(report, f, indent=4, ensure_ascii=False)
+    print("[+] JSON report generated: report.json")
+    print(json.dumps(report, indent=4, ensure_ascii=False))
+
+if __name__ == "__main__":
+    generate_report("sample.eml")
 
 def generate_report(file_path):
     mail = mailparser.parse_from_file(file_path)
